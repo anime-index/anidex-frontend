78c15abe079bae91bfe4d693662c5de21698f1d4
EstebanBatista <batista.esteban99@gmail.com>
2022-04-30 16:56:46 +0200

    Navigation Bar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <script setup>
 import "bootstrap/dist/css/bootstrap.min.css"
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
+
 </script>
+
 
 <template>
   <header>
-    <!--img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /-->
-
-    <div class="wrapper">
-      <!--HelloWorld msg="You did it!" /-->
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/topanime">Anime Top</RouterLink>
-      </nav>
-      <br>
+    <div class="global-nav">
+      <Navigation />
+      <RouterView />
     </div>
   </header>
 
-  <RouterView />
 </template>
 
+<script>
+import Navigation from "./components/Navigation";
+
+export default {
+  name: "App",
+  components: {
+    Navigation,
+  },
+};
+</script>
 <style>
 @import '@/assets/base.css';
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,72 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+<template>
+  <div class="home">
+    <section class="welcome">
+      <div class="ani-text container">
+        <h4>Your anime index</h4>
+        <hr />
+        <h2>
+          Welcome to <br />
+          Anidex
+        </h2>
+        <hr />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+};
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style lang="scss" scoped>
+.welcome {
+  background-image: url("../assets/bg_test.jpg");
+  background-attachment: fixed;
+  position: relative;
+  height: 100vh;
+
+  img {
+    object-fit: cover;
+    height: 100%;
+    width: 100%;
+  }
+
+  .ani-text {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    color: #fff;
+
+    h4 {
+      text-transform: uppercase;
+      font-size: 22px;
+      padding-bottom: 4px;
+    }
+
+    h2 {
+      font-size: 50px;
+      @media (min-width: 550px) {
+        font-size: 80px;
+      }
+    }
+
+    hr:nth-child(2) {
+      max-width: 365px;
+      margin-bottom: 16px;
+    }
+    hr:nth-child(4) {
+      height: 6px;
+      background-color: #fff;
+      border: none;
+      max-width: 85px;
+      margin-top: 16px;
+    }
+  }
+}
+</style>
