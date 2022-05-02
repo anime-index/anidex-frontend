772659adb3ea01ba8c86f546240a54aeb6d42cd1
Antonio Tigri <entertainmentatv@gmail.com>
2022-05-03 00:34:05 +0200

    Popover Images for anime finally

--- src/components/AnimeTableItem.vue
+++ src/components/AnimeTableItem.vue
@@ -3,6 +3,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import { Popover } from 'bootstrap/dist/js/bootstrap.esm.min.js'
+import { onUpdated } from '@vue/runtime-core';
 defineProps({
     position: Number,
     title: String,
@@ -16,15 +17,18 @@ defineProps({
 <script>
 export default {
     mounted() {
-        Array.from(document.querySelectorAll('button[data-bs-toggle="popover"]'))
-        .forEach(popoverNode => new Popover(popoverNode))
+        Array.from(document.querySelectorAll('[data-bs-toggle="popover"]'))
+        .forEach(popoverNode => new Popover(popoverNode, {html: true, content: function() {
+            console.log(this.getAttribute('data-img'))
+            return '<div class="media"><img class="img-fluid" src="' + this.getAttribute('data-img') + '"></div>'}
+            }))
     }
 }
 </script>
 
 
 <template>
-     <tr data-bs-container="body" data-bs-trigger="hover" data-bs-toggle="popover" data-bs-placement="left" :title="title" data-bs-content="Some Content">
+     <tr ref="tr" data-bs-container="body" data-bs-trigger="hover" data-bs-toggle="popover" data-bs-placement="left" :title="title" :data-img="imageurl">
       <th scope="row">{{position}}</th>
       <td>{{title}}</td>
       <td>{{score}}</td>
