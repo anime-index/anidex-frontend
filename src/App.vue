768f4db9f5464872a414db767e6fafbf6ed70bb7
Antonio Tigri <entertainmentatv@gmail.com>
2022-05-04 00:20:04 +0200

    List Edition Added (Needs Logic)

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import "@popperjs/core"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
+import "bootstrap-icons/font/bootstrap-icons.css"
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from '@/components/HelloWorld.vue'
 </script>
