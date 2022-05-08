caf58057e360e2d45f377aba6747ad8d8b03b13b
EstebanBatista <batista.esteban99@gmail.com>
2022-05-08 22:02:05 +0200

    Stilo + iconos

--- src/App.vue
+++ src/App.vue
@@ -10,15 +10,16 @@ import { RouterLink, RouterView } from 'vue-router'
   <div>
     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #35495e">
       <div id="topappbar" class="container-fluid" >
-        <a class="navbar-brand" href="#">Anidex</a>
+        <a class="navbar-brand" href="#">
+          <RouterLink class="nav-link active" to="/">
+            <img class="homelogo" src="assets/anidex_logo.png" alt="Anidex" height="50" width="50">
+          </RouterLink>
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-                            <RouterLink class="nav-link active" to="/">Home</RouterLink>
-            </li>
             <li class="nav-item">
                             <RouterLink class="nav-link active" to="/list">Lists</RouterLink>
             </li>
@@ -158,5 +159,9 @@ a {
 }
 a:hover {
   color: #3e3e56
+}
+
+.homelogo{
+  ;
 }
 </style>
