2578b531e2a7c34324113e471a0d3ad1b12d3506
Andreu Vall Hernàndez <andreu.vallhernandez@gmail.com>
2022-05-09 17:32:45 +0200

    No warning please

--- src/views/TopSeriesView.vue
+++ src/views/TopSeriesView.vue
@@ -32,7 +32,6 @@ export default {
 	mounted() {
 		this.select_page(0)
         var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-        console.log(popoverTriggerList)
         var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
             return new Popover(popoverTriggerEl, {html: true, delay: {hide: 5000}, continer: 'body'})
         })
@@ -42,15 +41,17 @@ export default {
 
 <template>
 
-	<p>
-		<button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-			What is a Series?
-		</button>
-	</p>
-	<div class="collapse" id="collapseExample">
-		<div class="card card-body">
-			A Series correspond to the entire Anime Series. It has multiple seasons associated to it in cronological order.
-			If you want to see Anime seasons separated, check instead <RouterLink class="dropdown-item" to="/top/anime">Top of Animes</RouterLink>
+	<div>
+		<p>
+			<button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+				What is a Series?
+			</button>
+		</p>
+		<div class="collapse" id="collapseExample">
+			<div class="card card-body">
+				A Series correspond to the entire Anime Series. It has multiple seasons associated to it in cronological order.
+				If you want to see Anime seasons separated, check instead <RouterLink class="dropdown-item" to="/top/anime">Top of Animes</RouterLink>
+			</div>
 		</div>
 	</div>
 
@@ -60,15 +61,15 @@ export default {
 		
         <Transition name="fade" class="mine">
 			<div v-if="place_holder">
-			<span data-bs-toggle="popover" data-bs-trigger="hover focus"
-            data-bs-title="Ups! The backend is down"
-            data-bs-content="Please go to <a href='https://github.com/anime-index/anidex-backend'>Anidex Backend">
-				<div class="container">
-					<div class="row gy-3">
-						<SeriesPlaceholder v-for="i in 30" :key="i"/>
+				<span data-bs-toggle="popover" data-bs-trigger="hover focus"
+				data-bs-title="Ups! The backend is down"
+				data-bs-content="Please go to <a href='https://github.com/anime-index/anidex-backend'>Anidex Backend">
+					<div class="container">
+						<div class="row gy-3">
+							<SeriesPlaceholder v-for="i in 30" :key="i"/>
+						</div>
 					</div>
-				</div>
-			</span>
+				</span>
 			</div>
         </Transition>
 
