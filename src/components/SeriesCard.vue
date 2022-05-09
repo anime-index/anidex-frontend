4953c5bb2b1a22f48d949f91f92ab959b90da604
Andreu Vall Hernàndez <andreu.vallhernandez@gmail.com>
2022-05-09 16:43:19 +0200

    Why no fade?

--- src/components/SeriesCard.vue
+++ src/components/SeriesCard.vue
@@ -15,7 +15,7 @@ defineProps({
 
 <template>
     <div class="col">
-        <div class="card mycard myfade">
+        <div class="card mycard">
             <span class="position-absolute top-0 start-0 translate-middle badge rounded-circle bg-success mybadge">#{{position}}</span>
             <div class="row">
                 <div class="col myfixed">
