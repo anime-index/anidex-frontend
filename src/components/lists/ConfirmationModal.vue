2a53356ec150738acf982bb3a61f674d2aae0b72
Antonio Tigri <entertainmentatv@gmail.com>
2022-05-23 13:54:56 +0200

    final style adjusment?

--- src/components/lists/ConfirmationModal.vue
+++ src/components/lists/ConfirmationModal.vue
@@ -22,9 +22,48 @@ defineProps({
                 </div>
                 <div class="modal-footer">
                     <button class="btn btn-primary" :data-bs-target="'#'+prevModalId" data-bs-toggle="modal">No</button>
-                    <button class="btn btn-secondary" @click="$emit('confirmed')" data-bs-dismiss="modal">Yes</button>
+                    <button class="btn btn-transparent" @click="$emit('confirmed')" data-bs-dismiss="modal">Yes</button>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+
+.modal-header {
+    background-color: #130230;
+    color: white;
+    font-family: "Montserrat-bold";
+}
+.modal-content {
+    background-color: #000021;
+    color: white;
+}
+
+.modal-footer {
+    background-color: #130230;
+}
+
+.btn-primary {
+    background-color: #69b549;
+    border: #69b549;
+}
+
+.btn-primary:hover {
+    background-color: #368519;
+    border: #368519;
+}
+
+.btn-transparent {
+    color: #fff;
+    margin-right: 8px;
+}
+
+.btn-transparent:hover {
+    color: #fff;
+    background-color: #3a2c5a;
+}
+
+
+</style>
